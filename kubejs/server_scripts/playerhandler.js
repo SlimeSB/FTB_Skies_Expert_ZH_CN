06de4b1e525f280a83e0badddc7864cddd931955
Addyabbia <1121333722@qq.com>
2024-03-05 02:10:25 +0800

    更新 playerhandler.js

--- kubejs/server_scripts/playerhandler.js
+++ kubejs/server_scripts/playerhandler.js
@@ -219,6 +219,8 @@ PlayerEvents.inventoryChanged(event => {
 	) {
 		swapItem(player, item.id.toString(), 'ftbskies:eye_of_legend')
 	}
+	if (item.id == 'ftbskies:mana_steel_mesh' && !player.stages.has('mana_steel_mesh'))
+		player.stages.add('mana_steel_mesh')
 })
 //easter egg
 ItemEvents.rightClicked('ftbskies:easter_egg', event => {
@@ -255,6 +257,41 @@ ItemEvents.rightClicked('ftbskies:easter_egg', event => {
 	}
 })
 
+BlockEvents.rightClicked('occultism:storage_controller', event => {
+	const { player, block, item } = event
+	if (item.id == 'ars_nouveau:dominion_wand') {
+		error(event, '不能在存储促动器上使用该魔杖')
+		event.cancel()
+	}
+})
+const lecternTile = Java.loadClass(
+	'com.hollingsworth.arsnouveau.common.block.tile.StorageLecternTile'
+)
+BlockEvents.rightClicked('ars_nouveau:storage_lectern', event => {
+	const { player, block, item, level } = event
+	const storageControllers = []
+
+	let currLectern = lecternTile(level.getBlockEntity(block.pos))
+	let mainLectern = currLectern.getMainLectern()
+	let connectedInventories = mainLectern.connectedInventories
+
+	// check for storage controller
+	connectedInventories.forEach(inventory => {
+		let block = level.getBlock(inventory)
+		if (block.id == 'occultism:storage_controller') {
+			error(event, '检测到存储促动器，从网络中移除...')
+			storageControllers.push(inventory)
+		}
+	})
+
+	storageControllers.forEach(controller => {
+		connectedInventories.remove(controller)
+	})
+	if (storageControllers.length > 0) {
+		event.cancel()
+	}
+})
+
 //Player tick handler
 PlayerEvents.tick(event => {
 	const { player, server, level } = event
@@ -267,15 +304,17 @@ PlayerEvents.tick(event => {
 
 	//prevents all of these update checks from happening too often
 	if (pData.gameTimer % 80 != 0) return
-
-	if (
-		!player.stages.has('got_pearl') &&
-		getRandomInt(1, 100) < 15 &&
-		level.dimension != 'minecraft:overworld'
-	) {
-		try_enderman_spawn(player, level)
-	}
 	pData.gameTimer = 0
+
+	if (player.stages.has('trigger_enderman')) {
+		if (
+			!player.stages.has('got_pearl') &&
+			getRandomInt(1, 100) < 15 &&
+			level.dimension != 'minecraft:overworld'
+		) {
+			try_enderman_spawn(player, level)
+		}
+	}
 
 	// seen blaze event
 	if (!player.stages.has('seen_blaze')) {
@@ -338,6 +377,35 @@ function try_enderman_spawn(player, level) {
 	}
 }
 
+BlockEvents.rightClicked('create:hand_crank', event => {
+	let canceled = false
+	let player = event.entity
+	if (!player.isPlayer()) canceled = true
+	if (player.isFake()) canceled = true
+	if (canceled === true) {
+		event.block.offset(event.facing).popItem(Item.of('create:hand_crank'))
+		event.block.set('minecraft:air')
+		event.cancel()
+	}
+})
+
+BlockEvents.broken(event => {
+	const { player, block, level } = event
+	let kuLevel = new Ku.Level(level)
+	let blockPos = new BlockPos(block.pos.x, block.pos.y, block.pos.z)
+	if (blockPos.getBlock().hardness >= 2) {
+		if (
+			kuLevel.isStructureAtLocation(
+				blockPos,
+				'castle_in_the_sky:castle_in_the_sky'
+			)
+		) {
+			event.cancel()
+			message(event, '天空城堡的力量太强大，无法突破')
+		}
+	}
+})
+
 BlockEvents.placed(
 	[
 		'ftbskies:force_infused_moon_stone',
@@ -353,11 +421,48 @@ BlockEvents.placed(
 
 		if (event.player) {
 			let blockName = Text.translate(block.item.descriptionId).string
-			error(event, `您只能将 ${blockName} 放在轨道空间中！`)
+			error(event, `您只能将 ${blockName} 放置在轨道空间中！`)
 		}
 		event.cancel()
 	}
 )
+
+BlockEvents.placed(['minecraft:end_portal_frame'], event => {
+	const { block } = event
+	let dimension = block.level.dimension
+	if (dimension == 'ad_astra:glacio') return
+
+	if (event.player) {
+		let blockName = Text.translate(block.item.descriptionId).string
+		error(event, `您只能将 ${blockName} 放置在霜原上`)
+	}
+	event.cancel()
+})
+
+PlayerEvents.loggedIn(event => {
+	const { player, server } = event
+
+	if (event.level.dimension == 'minecraft:overworld') return
+	player.stages.remove('in_prison')
+	if (player.stages.has('special_player')) return
+
+	let username = player.username.toLowerCase()
+	if (Object.keys(global.specialPlayers).includes(username)) {
+		player.stages.add('special_player')
+		player.tell(global.specialPlayers[username].message)
+
+		global.specialPlayers[username].items.forEach(item => {
+			player.give(item)
+		})
+	}
+})
+
+EntityEvents.hurt('minecraft:player', event => {
+	const { entity, source } = event
+	if (entity.stages.has('in_prison') && source.getActual().type == 'minecraft:enderman') {
+		event.cancel()
+	}
+})
 
 // liquid source explosion WIP
 // ItemEvents.rightClicked("starbunclemania:source_fluid_bucket", (event) => {
